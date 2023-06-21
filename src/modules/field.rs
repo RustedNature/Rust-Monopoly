ecb547ac425cb9c0c6f6399429e1c2979f48c6bd
Nico <133128777+RustedNature@users.noreply.github.com>
2023-06-21 19:34:01 +0200

    Organized fields

--- src/modules/field.rs
+++ src/modules/field.rs
@@ -1,7 +1,24 @@
-use crate::modules::buyable_field::BuyableField;
-use crate::modules::general_field::GeneralField;
+use std::hash::Hash;
 
+mod buyable_field;
+mod general_field;
+
+use buyable_field::BuyableField;
+use general_field::GeneralField;
 pub enum Field {
     General(GeneralField),
     Buyable(BuyableField),
+}
+
+impl Eq for Field {}
+
+impl PartialEq for Field {
+    fn eq(&self, other: &Self) -> bool {
+        self == other
+    }
+}
+impl Hash for Field {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        std::mem::discriminant(self).hash(state);
+    }
 }
