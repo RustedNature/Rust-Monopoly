26c9e872480b9cee669d1ed1cff407c27cd9b4de
Nico <133128777+RustedNature@users.noreply.github.com>
2023-06-24 18:21:18 +0200

    Renamed

--- src/modules/constants.rs
+++ src/modules/constants.rs
@@ -1,4 +1,4 @@
-pub mod constants {
+pub mod constant {
     pub const PLAYER_START_MONEY: i32 = 1500;
     pub const GO_FIELD: u32 = 0;
     pub const JAIL_FIELD: u32 = 10;
@@ -6,4 +6,11 @@ pub mod constants {
     pub const CNT_OF_FIELDS_ON_BOARD: u32 = 39;
     pub const MIN_PLAYERS: u32 = 2;
     pub const MAX_PLAYERS: u32 = 8;
+    pub const PLAYER_ACTIONS: [&str; 5] = [
+        "Würfeln",
+        "Handeln",
+        "Häuser kaufen",
+        "Häuser verkaufen",
+        "Felder beleihen",
+    ];
 }
